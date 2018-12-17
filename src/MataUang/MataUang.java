9405ba8eb800a2998ed616eec57a8c74094114c2
Za <salma.azizah@students.amikom.ac.id>
2018-12-17 16:13:37 +0700

    Menambahkan Scanner

--- src/MataUang/MataUang.java
+++ src/MataUang/MataUang.java
@@ -1,20 +1,34 @@
 package MataUang;
-
+import java.util.Scanner;
 public class MataUang {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		import javax.swing.JOptionPane;
-		public class MataUang {
-		public static void main (String args []){
-			double rupiah;
-        String r =javax.swing.JOptionPane.showInputDialog("Masukkan nilai Dollar ");//Digunakan untuk menampilkan pop up Masukkan nilai Dollar
-        double r1 = Double.parseDouble(r); //perintah konversi dari string ke double
-        rupiah=r1*14000;//digunakan untuk eksekusi nilai dollar ke rupiah.
+		 int  pilihan;
+		  System.out.print("Masukkan nilai tukar Dollar ke Rupiah : ");
+		  Scanner dolar = new Scanner(System.in);
+		  int nilai_tukar = dolar.nextInt();
+		  System.out.print("Masukkan 0 (Dollar ke Rupiah) dan 1 (Rupiah ke Dollar): ");
+		  Scanner opsi = new Scanner(System.in);
+		  pilihan = opsi.nextInt();
 
-        JOptionPane.showMessageDialog(null, "Hasil konversi Mata Uang: \n dollar " +r+" = "+rupiah+" rupiah ");
+		   if (pilihan == 0) {
+		   System.out.print("Masukkan Dollar yang ingin ditukar : ");
+		   Scanner uang = new Scanner(System.in);
+		   int nominal = uang.nextInt();
+		   int dolarkerupiah = nominal * nilai_tukar;
+		   System.out.println("USD $" + nominal
+		     + " yang kamu tukar akan memperoleh Rp" + dolarkerupiah);
+		  } else if (pilihan == 1) {
+		   System.out.print("Masukkan Rupiah yang ingin ditukar: ");
+		   Scanner uang = new Scanner(System.in);
+		   double nominal1 = uang.nextDouble();
+		   double rupiahkedolar = nominal1 / nilai_tukar;
+		   System.out.println("Rp. " + nominal1
+		     + " yang kamu tukar akan memperoleh USD $" + rupiahkedolar);
+		  } else {
+		   System.out.print("Nilai yang anda inputkan Salah");
+		  }
+		 }
+		}
 
-	}
-	}
-}
-}
